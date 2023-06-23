3611f17ebc20dd0f3612be21e1f49e4872ff4fa6
Lindsey <linlidiaramirez@gmail.com>
2023-06-22 23:52:58 -0400

    changed img to image on about

--- src/components/About.js
+++ src/components/About.js
@@ -73,7 +73,7 @@ export default function About() {
                
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img
+            <Image
               className="object-cover object-center rounded"
               alt="me-sitting-down"
               src="./sideeye.png"
